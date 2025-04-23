21cc3e395c1967b2ddbccb0eabb119e0875b5d7b
deepak-nayak <deepaknayak55260@gmail.com>
2025-04-23 15:24:16 +0530

    Update SecurityConfig.java

--- JPA & Assignment/SpringBootAssignment/src/main/java/com/springboot/assignment/configs/SecurityConfig.java
+++ JPA & Assignment/SpringBootAssignment/src/main/java/com/springboot/assignment/configs/SecurityConfig.java
@@ -12,72 +12,64 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-// import org.springframework.security.core.userdetails.User;
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.security.core.userdetails.UserDetailsService;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-// import org.springframework.security.crypto.password.PasswordEncoder;
-// import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    
     @Autowired
     private UserDetailsService userDetailsService;
 
-    
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
 
                 .csrf(customizer -> customizer.disable())
                 .authorizeHttpRequests(request -> request
-                //7
-                    .requestMatchers("/admin/**","/save-user/**").hasRole("ADMIN")  // Only ADMIN can access
-                    .requestMatchers("/employees/**","/employee/**").hasAnyRole("USER", "ADMIN")  // Both USER and ADMIN can access
-                    .requestMatchers("/welcome-page/**").permitAll()  // Public access
-                    .anyRequest().authenticated())
+                        // 7
+                        .requestMatchers("/admin/**", "/save-user/**").hasRole("ADMIN") // Only ADMIN can access
+                        .requestMatchers("/employees/**", "/employee/**").hasAnyRole("USER", "ADMIN") // Both USER and
+                                                                                                      // ADMIN can
+                                                                                                      // access
+                        .requestMatchers("/welcome-page/**").permitAll() // Public access
+                        .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
-        //http.formLogin(Customizer.withDefaults());
+        // http.formLogin(Customizer.withDefaults());
 
     }
 
-    
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        //6
+        // 6
         provider.setPasswordEncoder(new BCryptPasswordEncoder(12));//
         provider.setUserDetailsService(userDetailsService);
         return provider;
     }
 
-                                   //Hardcoded User Details(UserName, Password and rules)//
+    // Hardcoded User Details(UserName, Password and rules)//
     // @Bean
     // public UserDetailsService userDetailsService() {
-    //     UserDetails user1 = User
-    //        .withUsername("deepak")
-    //        .password(passwordEncoder().encode("d@123")) // Encode password
-    //        .roles("USER")
-    //             .build();
-           
+    // UserDetails user1 = User
+    // .withUsername("deepak")
+    // .password(passwordEncoder().encode("d@123")) // Encode password
+    // .roles("USER")
+    // .build();
 
-    //     UserDetails user2 = User
-    //        .withUsername("swaraj")
-    //        .password(passwordEncoder().encode("s@123")) // Encode password
-    //        .roles("ADMIN")
-    //        .build();
+    // UserDetails user2 = User
+    // .withUsername("swaraj")
+    // .password(passwordEncoder().encode("s@123")) // Encode password
+    // .roles("ADMIN")
+    // .build();
 
-    //     return new InMemoryUserDetailsManager(user1, user2);
+    // return new InMemoryUserDetailsManager(user1, user2);
     // }
 
     // @Bean
     // public PasswordEncoder passwordEncoder() {
-    //     return new BCryptPasswordEncoder();
+    // return new BCryptPasswordEncoder();
     // }
 }
